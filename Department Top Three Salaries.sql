730c3524a7487a2f5abcc3d47b8c3ef929a84e16
Ayushi Sahu <151500542+Ayushi2006Sahu@users.noreply.github.com>
2026-08-09 17:01:58 +0530

    Add SQL query for top three salaries by department

---
+++ Department Top Three Salaries.sql
@@ -0,0 +1,12 @@
+
+select d.name as Department,
+        e1.name as Employee,
+        e1.salary as Salary
+From Employee e1
+Join Department d on e1.departmentId = d.id
+where 3>(
+    select count(distinct e2.salary)
+    from Employee e2
+    where e2.salary>e1.salary
+    and e2.departmentId = e1.departmentId
+);
